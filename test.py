94be18aa82a301bf24b47a525cc6e89c695be51a
Alex Lübeck <ndsvw@gmx.de>
2019-09-29 13:18:00 +0200

    added unit tests for union_vertices

---
+++ test.py
@@ -0,0 +1,81 @@
+import unittest
+
+from min_cuts import union_vertices, count_probable_min_cuts
+
+
+class MinCutsTest(unittest.TestCase):
+
+    def test_union_vertices(self):
+        # Test 1
+        G = [
+            [1, 2],
+            [0, 2],
+            [0, 1]
+        ]
+        union_vertices(G, 0, 1)
+        self.assertListEqual(G[0], [])
+        self.assertListEqual(G[1], [2, 2])
+        self.assertListEqual(G[2], [1, 1])
+
+        # Test 2
+        G = [
+            [1, 2],
+            [0, 2],
+            [0, 1]
+        ]
+        union_vertices(G, 0, 2)
+        self.assertListEqual(G[0], [])
+        self.assertListEqual(G[1], [2, 2])
+        self.assertListEqual(G[2], [1, 1])
+
+        # Test 3
+        G = [
+            [1, 2],
+            [0, 2],
+            [0, 1]
+        ]
+        union_vertices(G, 2, 1)
+        self.assertListEqual(G[0], [1, 1])
+        self.assertListEqual(G[1], [0, 0])
+        self.assertListEqual(G[2], [])
+
+        # Test 4
+        G = [
+            [1, 2],
+            [0, 3, 4],
+            [0],
+            [1, 4],
+            [1, 3]
+        ]
+        union_vertices(G, 1, 3)
+        self.assertSetEqual(set(G[0]), {3, 2})
+        self.assertListEqual(G[1], [])
+        self.assertListEqual(G[2], [0])
+        self.assertTrue(all([
+            G[3].count(4) == 2,
+            G[3].count(0) == 1,
+            len(G[3]) == 3
+        ]))
+        self.assertTrue(all([
+            G[4].count(3) == 2,
+            len(G[4]) == 2
+        ]))
+
+        # Test 5
+        G = [
+            [1, 2],
+            [0, 3, 4],
+            [0],
+            [1, 4],
+            [1, 3]
+        ]
+        union_vertices(G, 0, 2)
+        self.assertListEqual(G[0], [])
+        self.assertSetEqual(set(G[1]), {2, 3, 4})
+        self.assertListEqual(G[2], [1])
+        self.assertListEqual(G[3], [1, 4])
+        self.assertListEqual(G[4], [1, 3])
+
+
+if __name__ == "__main__":
+    unittest.main()
